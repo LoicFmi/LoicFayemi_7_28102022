1d787718f65a1292dfcae005186c288d3225549d
LoicFmi <90246399+LoicFmi@users.noreply.github.com>
2022-12-02 15:45:20 +0100

    RecipeCards

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,5 @@
 const mainSearch = document.querySelector(".search-input");
+const cardsSection = document.querySelector(".cards");
 
 let recipesArray = [];
 
@@ -18,7 +19,7 @@ async function getRecipes() {
         });
 }
 
-let cardsArray = [];
+let cardsArray = recipesArray;
 
 // Récupère les recettes correspondant à la recherche effectuée 
 async function mainSearchBar() {
@@ -45,15 +46,70 @@ async function mainSearchBar() {
 
     if (cardsArray.length !== 0) {
         console.log(cardsArray);
+        displayRecipes(cardsArray);
     } else {
-        console.log('Aucune recette ne correspond à votre critère…')
+        cardsSection.innerHTML = 'Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc';
     }
 }
 
 // Déclenche la recherche lorsque plus de 2 caractères sont entrés dans la barre de recherche principale
 mainSearch.addEventListener('keyup', function () {
     console.clear();
+    // Vide la section cards
+    cardsSection.innerHTML = "";
     if (mainSearch.value.length > 2) {
         mainSearchBar();
+    } else {
+        displayRecipes(recipesArray);
     }
 });
+
+async function displayRecipes(recipes) {
+
+    await getRecipes();
+
+    recipes.forEach((recipe) => {
+        let ingredientsList = "";
+
+        recipe.ingredients.map((e) => {
+            ingredientsList += `<li class="card_ingredient">
+              <span class="card_ingredient_bold">${e.ingredient ? e.ingredient.trim() : ""}</span> 
+              ${e.quantity ? e.quantity.toString().trim() : ""} ${e.unit ? e.unit.toLowerCase().trim() : ""}
+             </li>`;
+
+            return ingredientsList;
+        });
+
+        cardsSection.insertAdjacentHTML(
+            "afterbegin",
+            `<article class="card">
+                <a href="#">
+                    <div class="card_img"></div>
+                    <div class="card_body">
+                        <div class="card_head">
+                            <h2 class="card_title">${recipe.name.trim()}</h2>
+                            <div class="card_time">
+                                <img class="card_clock" src="../assets/clock.svg" alt="clock icon"/>
+                                <p class="card_minutes">${recipe.time.toString().trim()} min</p>
+                            </div>
+                        </div>
+                        <div class="card_content">
+                            <ul class="card_ingredients">${ingredientsList}</ul>
+                            <p class="card_description">${recipe.description.trim()}</p>
+                        </div>
+                    </div>
+                </a>
+            </article>`
+        );
+    });
+}
+
+async function init() {
+
+    await getRecipes();
+    displayRecipes(recipesArray);
+
+}
+
+init();
+// displayRecipes(recipesArray);
